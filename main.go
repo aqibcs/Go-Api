92b4b9f0787e0b6b1b8812f09f5b288a86ffa223
Aqib Shah <135546057+aqibcs@users.noreply.github.com>
2023-08-22 21:51:36 +0500

    Update main.go

--- main.go
+++ main.go
@@ -16,9 +16,10 @@ type book struct {
 }
 
 var books = []book{
-	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
-	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
-	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+	{ID: "1", Title: "Think and Grow Rich", Author: "Napoleon Hill", Quantity: 2},
+	{ID: "2", Title: "Brain on Fire", Author: "Susannah Cahalan", Quantity: 5},
+	{ID: "3", Title: "Rich Dad Poor Dad", Author: "Robert Kiyosaki", Quantity: 6},
+	{ID: "5", Title: "Atomic Habit", Author: "James Clear", Quantity: 9},
 }
 
 func getBooks(c *gin.Context) {
